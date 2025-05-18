a7f7a45fed2dd1f007ea5d84afbc57d7ed1d3560
Himanshu Shivhare <himanshushivhare@Himanshus-MacBook-Air.local>
2025-05-19 01:39:00 +0530

    All the kubernate files are running successfully

--- backend-healthcare/appointment-service/src/main/java/com/healthcare/appointmentservice/feignclients/DoctorFeignClient.java
+++ backend-healthcare/appointment-service/src/main/java/com/healthcare/appointmentservice/feignclients/DoctorFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "DOCTOR-SERVICE", url = "http://doctor-service:8082")
+@FeignClient(name = "DOCTOR-SERVICE") // , url = "http://doctor-service:8082"
 public interface DoctorFeignClient {
 
     @GetMapping("/doctor/findByName/{name}")
