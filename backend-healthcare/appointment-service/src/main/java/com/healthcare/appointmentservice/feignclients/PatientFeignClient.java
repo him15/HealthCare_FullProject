4c07647a72d6628385cf0a191f277321ba139c6a
Himanshu Shivhare <himanshushivhare@Himanshus-MacBook-Air.local>
2025-05-19 12:56:10 +0530

    updated feign client for appointment service

--- backend-healthcare/appointment-service/src/main/java/com/healthcare/appointmentservice/feignclients/PatientFeignClient.java
+++ backend-healthcare/appointment-service/src/main/java/com/healthcare/appointmentservice/feignclients/PatientFeignClient.java
@@ -4,7 +4,7 @@ import com.healthcare.appointmentservice.dtos.PatientDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "PATIENT-SERVICE") // , url = "http://patient-service:8083"
+@FeignClient(name = "PATIENT-SERVICE" , url = "${patient-service}") // , url = "http://patient-service:8083"
 public interface PatientFeignClient {
 
     @PostMapping("/patient/add")
